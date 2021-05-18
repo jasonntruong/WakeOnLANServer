a36b653daf239d2be3bfcd94940f488638369f87
jasont9923 <jason.truong@ryerson.ca>
2021-05-18 00:44:24 -0400

    Works as it should - adding encryption soon

--- wakeCompOnLan.php
+++ wakeCompOnLan.php
@@ -1,29 +1,32 @@
 <?php
+echo "Wake On LAN Server by Jason Truong";
 
-#Adds $computer parameter to database
+
+#Adds $computer parameter to database. Allows me to track where and when I used my Apache server to turn on my computer in an SQL database
+#DELETE THIS if you do not plan on setting up a database to store $computer values. If you do, please set up your data base on your own as this function only adds to the data base
 function addToDB($computer) {
     $servername = "localhost";
     $username = "username";
     $password = "password";
-    $databaseName = "myDB";
+    $databaseName = "myDataBase";
 
     #form new connection
     $connection = new mysqli($servername, $username, $password, $databaseName);
 
     if ($connection->connect_error) {
+        echo "Connection error";
         die("Connection failed: " . $connection->connect_error);
     }
-
-    #inserts computer values into ALLCOMPUTER table in database myDB
-    $sql = "INSERT INTO ALLCOMPUTER (compVal)
+    
+    $sql = "INSERT INTO computerValues (compVal)
     VALUES ('$computer')";
+
     if ($connection->query($sql) === TRUE) {
-        echo "Data added succesfully";
+        echo "Data added success";
     }
     else {
-        echo "Error adding to Database" . $connection->error;
+        echo "Error" . $connection->error;
     }
-
     $connection->close;
 }
 
@@ -38,7 +41,7 @@ function formatData($data) {
 #on GET web request for computer...
 if(isset($_GET['computer'])) {
     $computer = formatData($_GET['computer']);
-    addToDB($computer);         #add to database
+    addToDB($computer);         #add to database. again DELETE THIS if you do not plan on using the database
 
     #if GET web request computer == "FROM_GOOGLE_HOME"
     if (strcmp($computer,"FROM_GOOGLE_HOME") == 0) {
--- wakeCompOnLan.py
+++ wakeCompOnLan.py
@@ -4,4 +4,4 @@ from wakeonlan import send_magic_packet
 #values stored in Environment variables --> don't want to hand out my ip address :D
 PHYSADDRESS = os.environ["PC_PHYS_ADDRESS"]
 
-send_magic_packet(PHYSADDRESS)
+send_magic_packet(str(PHYSADDRESS))
